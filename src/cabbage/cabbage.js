47bbc94a137d8d09b182539133030db4ccbfe8a9
hdale94 <hakon.dale@outlook.com>
2025-10-08 18:23:09 +0200

    fix(main): updated Cabbage class file to newest version

--- src/cabbage/cabbage.js
+++ src/cabbage/cabbage.js
@@ -5,22 +5,24 @@
 console.log("Cabbage: loading cabbage.js");
 
 export class Cabbage {
-  
-  static sendParameterUpdate(message, vscode=null) {
+
+  static sendParameterUpdate(message, vscode = null) {
     const msg = {
       command: "parameterChange",
       obj: JSON.stringify(message)
     };
+    console.log("Cabbage.sendParameterUpdate:", message, "vscode:", vscode, "msg:", msg);
     if (vscode !== null) {
+      console.log("Sending via vscode.postMessage");
       vscode.postMessage(msg);
     }
     else {
-      console.log("Cabbage: sending parameter change from UI", msg);
+      console.log("Sending via window.sendMessageFromUI");
       window.sendMessageFromUI(msg);
     }
   }
 
-  static sendCustomCommand(command, vscode=null) {
+  static sendCustomCommand(command, vscode = null) {
     const msg = {
       command: command,
       text: JSON.stringify({})
@@ -29,16 +31,16 @@ export class Cabbage {
     if (vscode !== null) {
       vscode.postMessage(msg);
     }
-    else {      
+    else {
       window.sendMessageFromUI(msg);
     }
-  } 
+  }
 
-  static sendWidgetUpdate(widget, vscode=null) {
+  static sendWidgetUpdate(widget, vscode = null) {
     console.log("Cabbage: sending widget update from UI", widget.props);
     const msg = {
       command: "widgetStateUpdate",
-      obj:JSON.stringify(widget.props)
+      obj: JSON.stringify(CabbageUtils.sanitizeForEditor(widget))
     };
     if (vscode !== null) {
       vscode.postMessage(msg);
@@ -48,7 +50,7 @@ export class Cabbage {
     }
   }
 
-  static sendMidiMessageFromUI(statusByte, dataByte1, dataByte2, vscode=null) {
+  static sendMidiMessageFromUI(statusByte, dataByte1, dataByte2, vscode = null) {
     var message = {
       "statusByte": statusByte,
       "dataByte1": dataByte1,
@@ -69,6 +71,35 @@ export class Cabbage {
     }
   }
 
+  static sendChannelData(channel, data, vscode = null) {
+    var message = {
+      "channel": channel
+    };
+
+    // Determine if data is a string or number and set appropriate property
+    if (typeof data === "string") {
+      message.stringData = data;
+    } else if (typeof data === "number") {
+      message.floatData = data;
+    } else {
+      console.warn("Cabbage: sendChannelData received unsupported data type:", typeof data);
+      return;
+    }
+
+    const msg = {
+      command: "channelStringData",
+      obj: JSON.stringify(message)
+    };
+
+    console.log("Cabbage: sending channel data from UI", message);
+    if (vscode !== null) {
+      vscode.postMessage(msg);
+    }
+    else {
+      window.sendMessageFromUI(msg);
+    }
+  }
+
   static MidiMessageFromHost(statusByte, dataByte1, dataByte2) {
     console.log("Cabbage: Got MIDI Message" + statusByte + ":" + dataByte1 + ":" + dataByte2);
   }
@@ -80,6 +111,24 @@ export class Cabbage {
 
     const msg = {
       command: "fileOpen",
+      obj: JSON.stringify(message)
+    };
+    if (vscode !== null) {
+      vscode.postMessage(msg);
+    }
+    else {
+      window.sendMessageFromUI(msg);
+    }
+  }
+
+  static openUrl(vscode, url, file) {
+    var message = {
+      "url": url,
+      "file": file
+    };
+
+    const msg = {
+      command: "openUrl",
       obj: JSON.stringify(message)
     };
     if (vscode !== null) {
